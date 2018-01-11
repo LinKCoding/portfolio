6dc7fc5c0bebc61d22aba316d051ee465dbe84fb
linkcoding <kenny.lin.91@gmail.com>
2018-01-11 00:27:28 -0500

    added all projects to list

--- src/projects/AllProjects.js
+++ src/projects/AllProjects.js
@@ -24,10 +24,31 @@ const AllProjects = [
       {
         link: "https://github.com/LinKCoding/js-allergies-frontend",
         name: "github square"},
-      {}
+      {
+        link: "https://github.com/LinKCoding/js-allergies-backend",
+        name: "github"
+      }
     ]
   },
-
+  {
+    title: "Kofetcher"
+    image: "kofetcher-thumbnail.png"
+    description: "Kofetcher was inspired by the food delivery and tech integration services like grubhub. It explores a niche venue of delivering coffee to users from their shop of choice. Users will post an order with a set gratuity and another group of users (Fetchers), can accept this request. Users and fetchers are matched based on location and the delivery status is updated between the two parties."
+    languages: ["Ruby", "Ruby on Rails", "MySQLite"]
+    icons: [
+      {
+        link: "https://github.com/LinKCoding/kofetch",
+        name: "github square"
+      },
+    ]
+  },
+  {
+    title:"IMDB Clone"
+    image: "imdbClone-thumbnail.png"
+    description: "  IMDB clone is exactly like it sounds. However, instead of a complete website, it is a CLI application which allows users to research through a database containing 5,000 movie titles. With a twist of humor, users can search through titles, ratings, and even get recommendations based on year released and a rating threshold."
+    languages: ["Ruby", "MySQLite"]
+    icons: [{link: "https://github.com/LinKCoding/imdb-clone", name: "github square"}]
+  }
 ]
 
 export default AllProjects
