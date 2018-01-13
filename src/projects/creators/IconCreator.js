66277b6f7b10a74c8a3138f8704c94385a47ce68
linkcoding <kenny.lin.91@gmail.com>
2018-01-12 23:05:05 -0500

    remove distracting console logs

--- src/projects/creators/IconCreator.js
+++ src/projects/creators/IconCreator.js
@@ -2,8 +2,6 @@ import React from 'react'
 import { Icon } from 'semantic-ui-react'
 
 const IconCreator = (props) => {
-  console.log(props, "this is props");
-  console.log(props.icon);
   const { icon } = props
   return(
     <a href={icon.link} target="_blank" rel="noopener noreferrer">
