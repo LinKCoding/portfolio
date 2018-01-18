8e95a83e35dad7390d153a42ba29d3980f26e83b
linkcoding <kenny.lin.91@gmail.com>
2018-01-18 13:03:19 -0500

    start on mobile responsiveness

--- src/Bio.js
+++ src/Bio.js
@@ -3,7 +3,7 @@ import { Grid } from 'semantic-ui-react'
 
 const Bio = () => {
   return(
-    <Grid.Row>
+    <Grid.Row >
       <Grid.Column width={8}>
         <p id="bio"> Lifelong learner and educator <br/> Integrating web development with education </p>
       </Grid.Column>
