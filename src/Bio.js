1fa9858d462519b3d7518ea11603bf276648bac7
linkcoding <kenny.lin.91@gmail.com>
2018-01-24 19:53:27 -0500

    fixed typo on bio

--- src/Bio.js
+++ src/Bio.js
@@ -6,7 +6,7 @@ const Bio = () => {
     <Grid.Row >
       <Grid.Column width={8}>
         <p id="bio"> Lifelong learner and educator <br/> Integrating web development with education </p>
-        <p> If you want to work together or just have a chat, just click an icon below</p>
+        <p> If you want to work together or just have a chat, click an icon below</p>
       </Grid.Column>
     </Grid.Row>
   )
